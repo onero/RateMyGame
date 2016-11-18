ba9ed033c95ed79f802558764b2992bc3af9f5dc
Adam Hansen <adamlars90@gmail.com>
2016-11-18 09:24:29 +0100

    Updated design

--- src/ratemygame/gui/controller/RateMyGameMainViewController.java
+++ src/ratemygame/gui/controller/RateMyGameMainViewController.java
@@ -10,18 +10,42 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 
 /**
  *
- * @author gta1
+ * @author Adamino
  */
 public class RateMyGameMainViewController implements Initializable {
 
-    @FXML
     private Label label;
-
     @FXML
+    private TextField txtDescription;
+    @FXML
+    private Button btnOpen;
+    @FXML
+    private Button btnSave;
+    @FXML
+    private TableColumn<?, ?> tableDescription;
+    @FXML
+    private TableColumn<?, ?> tableRate;
+    @FXML
+    private TextField txtRate;
+    @FXML
+    private TextField txtHighestRated;
+    @FXML
+    private TextField txtLowestRated;
+    @FXML
+    private TextField txtAverage;
+    @FXML
+    private LineChart<?, ?> chart;
+    @FXML
+    private Button btnClear;
+
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
