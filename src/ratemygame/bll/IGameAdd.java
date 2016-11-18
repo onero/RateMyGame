1200dbf516e8b58dbbc1809e02993c9c3cfe6516
Adam Hansen <adamlars90@gmail.com>
2016-11-18 09:49:28 +0100

    Added abstract method for IGameAdd

--- src/ratemygame/bll/IGameAdd.java
+++ src/ratemygame/bll/IGameAdd.java
@@ -3,9 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ratemygame.bll;
 
-public class IGameAdd {
+public abstract class IGameAdd {
+
+    /**
+     * Add a game to
+     *
+     * @param description
+     * @param rating
+     */
+    public abstract void addGameRating(String description, double rating);
 
 }
